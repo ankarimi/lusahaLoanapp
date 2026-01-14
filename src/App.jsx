303a6a271768005bcd79647b258422b353739d3d
karimiarrison <karimiarrison@gmail.com>
2026-01-14 11:12:27 +0300

    cleanup: remove app code; preserve firebase & uploadImage and configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,10 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
-import { ThemeProvider } from "./context/ThemeContext";
-import ProtectedRoute from "./components/ProtectedRoute";
-import AdminRoute from "./components/AdminRoute";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import VerifyEmail from "./pages/VerifyEmail";
-import Onboarding from "./pages/Onboarding";
-import CustomerLayout from "./layouts/CustomerLayout";
-import AdminLayout from "./layouts/AdminLayout";
-import ForgotPassword from "./pages/ForgotPassword";
-import ChangePassword from "./pages/ChangePassword";
+import React from "react";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider>
-        <AuthProvider>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/verify-email" element={<VerifyEmail />} />
-            <Route path="/onboarding" element={<Onboarding />} />
-
-            {/* User area */}
-            <Route
-              path="/app/change-password"
-              element={
-                <ProtectedRoute>
-                  <ChangePassword />
-                </ProtectedRoute>
-              }
-            />
-
-            <Route
-              path="/app/*"
-              element={
-                <ProtectedRoute>
-                  <CustomerLayout />
-                </ProtectedRoute>
-              }
-            />
-
-            {/* Admin area */}
-            <Route
-              path="/admin/*"
-              element={
-                <AdminRoute>
-                  <AdminLayout />
-                </AdminRoute>
-              }
-            />
-          </Routes>
-        </AuthProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <div style={{ padding: 24 }}>
+      <h1>New app scaffold</h1>
+      <p>Firebase and uploadImage logic are preserved. Start building your new app.</p>
+    </div>
   );
 }
--- src/config/support.js
+++
@@ -1,4 +0,0 @@
-// Centralized support contacts
-export const WHATSAPP_NUMBER = "+254762634893";
-export const SUPPORT_EMAIL = "moderation.mails.go@gmail.com";
-export const ADMIN_EMAIL = SUPPORT_EMAIL;
